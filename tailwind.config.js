7334be01276a2639ccf967bec2cbf5e446c97e6d
Geetu94 <aj20001200@gmail.com>
2024-12-03 22:06:13 +0530

    Fix issue with password validation on signup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF0000",
+        secondary: "#FF0000",
+        neutral: "#FF0000",
+        info: "#FF0000",
+        success: "#FF0000",
+        warning: "#FBBD23",
+        error: "#FF0000",
+      },
+    },
+  },
+  plugins: [],
+};
